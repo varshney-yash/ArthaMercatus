afeb9a3a6e2562009a2b7e045578e23981abe892
py3-dev <[200~yash.varshney@careers360.in>
2024-01-18 06:57:09 +0530

    feat: integrate header, footer
    add react-bootstrap package

--- frontend/artha/src/App.js
+++ frontend/artha/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <div>
+        <Navbar />
+        <Container>
+          <h1>Welcome, this website is powered by DRF and React</h1>
+        </Container>
+        <Footer />
+      </div>
+    </>
+  );
 }
